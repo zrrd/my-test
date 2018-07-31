52f6d053f6bd0a57269a22d7136f2d0b96b15d96
shaoyijiong <shaoyijiong@worken.com>
2018-07-31 23:22:26 +0800

    Jsoup

--- src/main/java/com/example/mytest/jsoup/JsoupTest.java
+++ src/main/java/com/example/mytest/jsoup/JsoupTest.java
@@ -1,22 +1,74 @@
 package com.example.mytest.jsoup;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
- * 测试jsoup
+ * 测试jsoup.
  *
  * @author 邵益炯
  * @date 2018/7/31
  */
 public class JsoupTest {
 
+  private static final Integer WIDTH = 300;
+  private static final Integer HEIGHT = 300;
+  private static final String HTTP = "http:";
+  private static final String DEFAULT_IMAGE = "http://pic1.win4000.com/wallpaper/3/59783bb506c6e.jpg";
+  private static final String USER_AGENT = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16";
+
+  private static boolean getImgSize(String src, Integer width, Integer height) {
+    BufferedImage sourceImg = null;
+    try {
+      URL url = new URL(src);
+      // 根据url获取BufferImage对象
+      sourceImg = ImageIO.read(url);
+      // 获取图片的宽度和高度
+      Integer w = sourceImg.getWidth();
+      Integer h = sourceImg.getHeight();
+      if (w > width && h > height) {
+        return true;
+      }
+    } catch (IOException e) {
+      // TODO 打日志 而不是打控制台
+      System.out.println("获取图片失败");
+    }
+    return false;
+  }
+
+  /**
+   * 测试.
+   */
   public static void main(String[] args) throws Exception {
     String html = "http://news.163.com/photoview/00AO0001/2295329.html#p=DO1KF9T400AO0001NOS";
-    Document doc = Jsoup.connect(html).get();
+    String html2 = "https://weibo.com/?category=novelty";
+    //将请求模拟成手机访问
+    Document doc = Jsoup.connect(html2).userAgent(USER_AGENT).timeout(3000).get();
     System.out.println(doc.title());
-    //System.out.println(doc);
+    Elements elements = doc.select("img[src~=(?i)\\.(png|jpe?g)]");
+    //需要筛选后缀吗 Elements elements = doc.select("img[src]")
+    for (Element element : elements) {
+      String src = element.attr("src");
+      if (StringUtils.isNoneBlank(src)) {
+        //有的url缺少http前缀
+        if (!src.contains(HTTP)) {
+          src = HTTP + src;
+        }
+        if (getImgSize(src, WIDTH, HEIGHT)) {
+          System.out.println("符合 " + src);
+          return;
+        }
+      }
+    }
+    System.out.println("返回默认图片 " + DEFAULT_IMAGE);
   }
+
 
 }
--- src/main/java/com/example/mytest/lambda/LambdaTest.java
+++ src/main/java/com/example/mytest/lambda/LambdaTest.java
@@ -24,23 +24,23 @@ public class LambdaTest {
     String[] strings = new String[]{"a","b","c","a","ok"};
     List<String> list = Arrays.asList(strings);
 
-    //通过流去重
+    //通过流去重 distinct
     List list1 = list.stream().distinct().collect(Collectors.toList());
     System.out.println(list1);
 
-    //跳过前几个
+    //跳过前几个 skip
     List list2 = list.stream().skip(3).collect(Collectors.toList());
     System.out.println(list2);
 
-    //截取前几个
+    //截取前几个 limit
     List list3 = list.stream().limit(3).collect(Collectors.toList());
     System.out.println(list3);
 
-    //过滤对象
+    //过滤对象 filter
     List list4 = list.stream().filter("a"::equals).collect(Collectors.toList());
     System.out.println(list4);
 
-    //通过类型进行转换 如果字母为a 转化成 z 其他不变  (一行内不用写返回值)
+    //通过类型进行转换 如果字母为a 转化成 z 其他不变  (一行内不用写返回值) map
     List list5 = list.stream().map(b -> "a".equals(b) ? "z" : b).collect(Collectors.toList());
     System.out.println(list5);
 
@@ -50,11 +50,11 @@ public class LambdaTest {
     List list6 = list.stream().map(c -> c.split("")).flatMap(Arrays::stream).collect(Collectors.toList());
     System.out.println(list6);
 
-    //排序  随便一个排序
+    //排序  随便一个排序 sorted
     List list7 = list.stream().sorted((d,e)-> -(d.hashCode() - e.hashCode())+10).collect(Collectors.toList());
     System.out.println(list7);
 
-    //对每个对象执行提供的action操作
+    //对每个对象执行提供的action操作 peek
     List list8 = list.stream().peek(f -> System.out.println(f + "peek")).collect(Collectors.toList());
     //System.out.println(list8);
 
@@ -63,11 +63,25 @@ public class LambdaTest {
     System.out.println(list.stream().min(Comparator.naturalOrder()).orElse(""));
     System.out.println(list.stream().count());
 
-    //匹配
+    //匹配 anyMatch allMatch noneMatch
     //有匹配OK的
     System.out.println("匹配");
     System.out.println(list.stream().anyMatch(g -> "ok".equals(g)));
     System.out.println(list.stream().allMatch(g -> "ok".equals(g)));
     System.out.println(list.stream().noneMatch(g -> "pp".equals(g)));
+
+    //collect 数据结构的转换
+    list.stream().collect(Collectors.toList());
+
+    //reduce 把流进行操作后返回 返回值要和传入值的类型一致
+    System.out.println(list.stream().reduce((h,k) -> h + k).orElse(""));
+
+    //toArray 返回为一个object数组
+    String[] strings1 = (String[]) list.stream().toArray();
+
+    //forEach 对每个数据进行操作
+    list.forEach(System.out::println);
+
+    //findFirst  findAny
   }
 }
