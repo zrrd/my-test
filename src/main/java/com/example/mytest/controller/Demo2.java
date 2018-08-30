dd88695ede60aee58f0d9dce392ed9f8b7245425
邵益炯 <shaoyijiong@worken.cn>
2018-08-30 17:08:50 +0800

    修改端口

---
+++ src/main/java/com/example/mytest/controller/Demo2.java
@@ -0,0 +1,13 @@
+package com.example.mytest.controller;
+
+/**
+ * @author 邵益炯
+ * @date 2018/8/23
+ */
+public class Demo2 {
+
+  public static void main(String[] args) {
+    String url2="http://www.toutiao.com/group/a6592704393257157127";
+  }
+}
+
--- src/test/java/com/example/mytest/MyTestApplicationTests.java
+++ src/test/java/com/example/mytest/MyTestApplicationTests.java
@@ -1,21 +1,12 @@
 package com.example.mytest;
 
-import org.apache.commons.lang3.ArrayUtils;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MyTestApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		Object[] subs={7,5,3,9,8,4};
-		Map<Object, Object> map= ArrayUtils.toMap(subs);
-	}
 
 }
