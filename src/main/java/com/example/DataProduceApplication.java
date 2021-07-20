e2e8ff715844afea97380760fe8ac56f20590af9
shaoyijiong <fantasy9417@163.com>
2021-07-20 23:12:47 +0800

    init

--- src/main/java/com/example/DataProduceApplication.java
+++ src/main/java/com/example/DataProduceApplication.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author shaoyijiong
  */
 @SpringBootApplication
-@MapperScan(value = "com.example.mapper")
 public class DataProduceApplication {
 
   public static void main(String[] args) {
